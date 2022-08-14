67ac6920b4274b4ba80780f9aa29d770301ef5c5
Felipe.Silva <felipe.allware@gmail.com>
2022-08-14 19:06:04 -0300

    Subindo primeira versão do projeto

---
+++ js/perguntas.js
@@ -0,0 +1,18 @@
+function activeDropdown(idDropdown){
+    if(idDropdown == ""){
+        return;
+    }
+
+    let descricao = document.getElementById(idDropdown).children[0].children[1];
+    let imagem = document.getElementById(idDropdown).children[0].children[0].children[1].children[0].children[0]
+    let display = window.getComputedStyle(descricao, null).getPropertyValue("display")
+
+    if(display == "none"){
+        $(descricao).css("display", "block");
+        $(imagem).prop("src", "../images/seta-cima.png")
+    }
+    else{
+        $(descricao).css("display", "none")
+        $(imagem).prop("src", "../images/seta-baixo.png")
+    }
+}
